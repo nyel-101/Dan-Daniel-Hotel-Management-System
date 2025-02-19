1e3a6b48b875a70106cc486ec969fe0250169c59
nyel-101 <dandevjr@gmail.com>
2025-02-19 13:48:06 +0000

    feat(core): decouple view templates

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf41a34ee14f45d08aeeafa226ea6178 = () => {
-  return '2026-02-28T17:04:18Z';
+const node_a0076b8535fe4356a2c81272e7c358bc = () => {
+  return '2025-02-19T13:48:06Z';
 };
