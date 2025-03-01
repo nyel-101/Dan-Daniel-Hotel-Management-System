6de023b9249057e1cedbcb311451d87049256deb
nyel-101 <dandevjr@gmail.com>
2025-03-01 14:13:54 +0000

    fix(utils): sanitize database index

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_feea2dcb5cc34a9683673f1b25c402a8 = () => {
-  return '2026-01-20T03:35:06Z';
+const node_c88d3d351da34226a8b96d9be44040d4 = () => {
+  return '2025-03-01T14:13:54Z';
 };
