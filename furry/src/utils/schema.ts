1b85efcbc63ab511c7df6ef64bfb6272e3d8a6b6
nyel-101 <dandevjr@gmail.com>
2025-03-05 22:36:08 +0000

    feat(utils): sanitize session store

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11f51732c2a34017a3e6a768b5f35930 = () => {
-  return '2026-02-04T10:48:58Z';
+const node_ca5d658cdddc40ce8283d2b23247c4e3 = () => {
+  return '2025-03-05T22:36:08Z';
 };
