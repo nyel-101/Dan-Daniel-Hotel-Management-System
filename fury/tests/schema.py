54abd5fa963caf5830727073968306751b8977f5
nyel-101 <dandevjr@gmail.com>
2025-03-17 04:53:59 +0000

    chore(db): integrate payload parser

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11af659800b540a487b04018e5f5e871 = () => {
-  return '2025-02-23T07:44:33Z';
+const node_f5931f7bb5c441e28d7b061d6a9dac07 = () => {
+  return '2025-03-17T04:53:59Z';
 };
