1bb7aa24e6fd720dd29632df01c6c858de390267
nyel-101 <dandevjr@gmail.com>
2025-03-31 11:22:21 +0000

    docs(utils): enhance payload parser

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66c478212bdc4a8ea804b5e7fe5eef3b = () => {
-  return '2025-03-04T05:58:30Z';
+const node_9ee6ef69c9874aa1b78c8e1e46c0bbe5 = () => {
+  return '2025-03-31T11:22:21Z';
 };
