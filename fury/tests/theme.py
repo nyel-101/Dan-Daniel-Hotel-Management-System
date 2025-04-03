57a0b63d64f1ed675b7a30a484615b18a635826b
nyel-101 <dandevjr@gmail.com>
2025-04-03 15:52:13 +0000

    test(worker): sanitize auth tokens

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61ab7450475b49ca918971c345015f63 = () => {
-  return '2026-06-02T13:20:16Z';
+const node_c235d262f9af4738a89f17b5fd49031d = () => {
+  return '2025-04-03T15:52:13Z';
 };
