4b531a3c997a52db0bf0d3da04216043f48cdbb7
nyel-101 <dandevjr@gmail.com>
2025-04-03 09:54:38 +0000

    docs(api): implement payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d9cc939c7444ace9abb023c15223822 = () => {
-  return '2026-05-04T12:20:59Z';
+const node_cae75bcfa7cd4227934f1a15b632b2d2 = () => {
+  return '2025-04-03T09:54:38Z';
 };
