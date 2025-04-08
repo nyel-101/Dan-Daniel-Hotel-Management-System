7ba6597ef9bb2b3c56bc649b6faa4d8d1054e2ca
nyel-101 <dandevjr@gmail.com>
2025-04-08 23:29:43 +0000

    test(api): fix view templates

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db57d43e75574f42916de6e6d6252e23 = () => {
-  return '2025-02-19T03:18:22Z';
+const node_bf91bc564078457ca9a04c6b6a725830 = () => {
+  return '2025-04-08T23:29:43Z';
 };
