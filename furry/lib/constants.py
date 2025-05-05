e4c9c5ce0be2d940007825ea8f50b7ca6e1be1dc
nyel-101 <dandevjr@gmail.com>
2025-05-05 16:03:29 +0000

    chore(auth): update payload parser

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df8f4ba74232481e9bc87c2d62fafe46 = () => {
-  return '2025-04-29T20:08:42Z';
+const node_5814281a58e9439f806b0ad973380c8d = () => {
+  return '2025-05-05T16:03:29Z';
 };
