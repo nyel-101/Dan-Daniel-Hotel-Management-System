2a17b7a178bd141cb07d8059c71dc9978fdf9ff0
nyel-101 <dandevjr@gmail.com>
2025-05-08 13:40:16 +0000

    perf(auth): update caching layer

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96aa6eea94dc4dd8b8ba40011929f7f8 = () => {
-  return '2026-03-04T01:49:26Z';
+const node_a4b318b19d4a445b931e3878d648ef8a = () => {
+  return '2025-05-08T13:40:16Z';
 };
