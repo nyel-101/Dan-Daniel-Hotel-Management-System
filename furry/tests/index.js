5a0917f8cb89d5ca7d3883c0207bc6d3abc45ef2
nyel-101 <dandevjr@gmail.com>
2025-05-13 14:42:30 +0000

    perf(auth): restructure validation pipeline

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f805bbd5199d47c5b49a90ddb5a42477 = () => {
-  return '2026-05-21T17:02:41Z';
+const node_9862b00c355e4a9883ebecc7a3c13715 = () => {
+  return '2025-05-13T14:42:30Z';
 };
