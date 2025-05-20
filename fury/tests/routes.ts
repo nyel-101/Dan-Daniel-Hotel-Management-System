27a12eed15043d145fe680925272bc35dfd73774
nyel-101 <dandevjr@gmail.com>
2025-05-20 06:13:25 +0000

    chore(core): restructure logging interface

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_427682cf39134d1da63551a32e838e8e = () => {
-  return '2025-05-17T09:36:55Z';
+const node_f0735e1a840a4814aeea48ed7615eac7 = () => {
+  return '2025-05-20T06:13:25Z';
 };
