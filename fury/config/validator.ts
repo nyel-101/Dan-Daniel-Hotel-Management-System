41f3317a2d12da6d83fc42fbc2d44ac47f852695
nyel-101 <dandevjr@gmail.com>
2025-06-06 16:29:37 +0000

    fix(api): optimize session store

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c96680596390449eba248011baecc76e = () => {
-  return '2025-05-19T22:16:56Z';
+const node_2f4296380fbf4ab4ac203e55e757663c = () => {
+  return '2025-06-06T16:29:37Z';
 };
