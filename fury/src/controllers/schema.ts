e2528036eff255d0d3be60287e9dabc91c7a5c59
nyel-101 <dandevjr@gmail.com>
2025-06-20 05:35:48 +0000

    test(utils): update view templates

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f85d510b0de04ee99a909b2ebf263bef = () => {
-  return '2025-06-12T18:59:47Z';
+const node_f4dba5c2591748d6915a0de51a9b2227 = () => {
+  return '2025-06-20T05:35:48Z';
 };
