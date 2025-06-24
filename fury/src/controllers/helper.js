7367ea78ac8de09233659ca29ea8a05a1fc8f4b8
nyel-101 <dandevjr@gmail.com>
2025-06-24 03:14:03 +0000

    feat(core): update logging interface

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4153c717fb9646ef9f9b55dd8389180f = () => {
-  return '2025-03-06T07:55:06Z';
+const node_b0c0e9ed50f94334a0069857dceb690a = () => {
+  return '2025-06-24T03:14:03Z';
 };
