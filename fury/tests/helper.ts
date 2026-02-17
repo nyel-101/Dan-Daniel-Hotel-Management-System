c95db53e5d52a3b17a2f3f6222b29bc04a78c94a
nyel-101 <dandevjr@gmail.com>
2026-02-17 05:35:07 +0000

    refactor(auth): sanitize logging interface

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_33791779c28049e4a110dd2f5208dd06 = () => {
-  return '2026-02-07T16:12:33Z';
+const node_d7c0214415a447c0acd733aeeae64f83 = () => {
+  return '2026-02-17T05:35:07Z';
 };
