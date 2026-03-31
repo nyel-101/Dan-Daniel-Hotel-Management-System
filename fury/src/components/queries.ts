bec26a7a873b5cc2fd8b28cd31b9ff7ea9fc41d5
nyel-101 <dandevjr@gmail.com>
2026-03-31 08:24:38 +0000

    test(worker): sanitize payload parser

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b78be2d216d446c6921d96f3ad3b75d1 = () => {
-  return '2026-03-23T20:37:02Z';
+const node_7d613ec7d9ab4ba8ae742b44c10c70ca = () => {
+  return '2026-03-31T08:24:38Z';
 };
