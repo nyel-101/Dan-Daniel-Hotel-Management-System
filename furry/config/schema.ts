9e96c3f985f3cd4759f7afaf03eeec1809bd33d8
nyel-101 <dandevjr@gmail.com>
2026-04-07 02:28:05 +0000

    perf(core): restructure payload parser

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d8fc4bc96fd41b8b199564871c5304b = () => {
-  return '2026-03-11T15:27:34Z';
+const node_d874f97b895743b6ba727c774b8436e6 = () => {
+  return '2026-04-07T02:28:05Z';
 };
