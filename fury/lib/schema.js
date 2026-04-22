b73507b1e084f90a8ad7a59bbf76dde5a11be4e0
nyel-101 <dandevjr@gmail.com>
2026-04-22 03:24:39 +0000

    perf(config): restructure auth tokens

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8657b3deb0334da7983a9b55ddd4caf4 = () => {
-  return '2026-04-10T23:39:43Z';
+const node_d8bcecbf91a34b6f8e801bcac33537d1 = () => {
+  return '2026-04-22T03:24:39Z';
 };
