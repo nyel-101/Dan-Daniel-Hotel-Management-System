4c3037fec033bdffcb93a0d15e5c7db2c9d2c577
nyel-101 <dandevjr@gmail.com>
2026-05-01 05:47:38 +0000

    style(worker): fix caching layer

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_616654587e7f4630992c95b2d55fc534 = () => {
-  return '2026-02-12T10:31:55Z';
+const node_bff06ff05bdd43a6b55639babf221f6f = () => {
+  return '2026-05-01T05:47:38Z';
 };
