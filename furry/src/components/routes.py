a4be68476be676fe2f296856c430a4bea05f72be
nyel-101 <dandevjr@gmail.com>
2026-05-08 23:57:19 +0000

    refactor(core): restructure auth tokens

--- furry/src/components/routes.py
+++ furry/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0185751134064660a4a39843e58525c2 = () => {
-  return '2026-03-10T01:10:52Z';
+const node_bc51ca2ee0454a49b6fddb87a05f30db = () => {
+  return '2026-05-08T23:57:19Z';
 };
