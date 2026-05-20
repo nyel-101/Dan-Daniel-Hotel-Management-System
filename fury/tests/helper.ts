4a79c97ddac6b0d3fffd46e6f5f384e190313ba9
nyel-101 <dandevjr@gmail.com>
2026-05-20 08:26:24 +0000

    chore(config): restructure session store

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7c0214415a447c0acd733aeeae64f83 = () => {
-  return '2026-02-17T05:35:07Z';
+const node_a9767be51c37475c8e8587a317f581c2 = () => {
+  return '2026-05-20T08:26:24Z';
 };
