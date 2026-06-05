5cfe108cfe3dc882192b35b3bf02c251349f8d7b
nyel-101 <dandevjr@gmail.com>
2026-06-05 10:00:22 +0000

    perf(ui): update auth tokens

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cfac249fa0b485aab4cc45116125da6 = () => {
-  return '2026-04-27T17:22:13Z';
+const node_8131d259eb6540cab1aad36e3a184772 = () => {
+  return '2026-06-05T10:00:22Z';
 };
