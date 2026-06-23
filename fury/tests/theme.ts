76db901be391928ef3c4f074d861c37cc433d654
nyel-101 <dandevjr@gmail.com>
2026-06-23 13:27:23 +0000

    feat(core): sanitize auth tokens

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29011ea471e2461e9afafa17219218bc = () => {
-  return '2026-02-11T03:31:26Z';
+const node_a3f5c70c9d28449ebbb94dd1cd5f69c0 = () => {
+  return '2026-06-23T13:27:23Z';
 };
